cbb017d71f73d5a952c6349fdf090b390ad67d6d
brian <brian.tria.se@gmail.com>
2020-11-27 03:41:11 +0400

    multiple spawnpoints

---
+++ Assets/LegoDodgeBall/Scripts/Trophy.cs
@@ -0,0 +1,26 @@
+/**
+ *  created by  : Brian Tria
+ *  date        : 27/11/2020 03:25:55
+ *  description :
+ **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.LEGO.Behaviours.Actions;
+
+namespace LegoDodgeBall
+{
+    public class Trophy : PickupAction
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            // if (m_Effect != null)
+            // {
+
+            // }
+        }
+    }
+}
